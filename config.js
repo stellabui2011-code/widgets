25e46c71c06ceea6653d2cd486f3fe027e9c8a38
stellabui2011-code <stellabui2011@gmail.com>
2026-06-15 16:47:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    clientId: 'client id here',
-    clientSecret: 'client secret here',
+    clientId: '1516085382280908921',
+    clientSecret: '-LelHLnqcp870v9xt5F_Ov5IEqQi7UZ0',
     redirectUri: 'http://localhost:3000/oauth2',
-    botToken: 'bot token here',
-    discordApplicationId: 'app id here',
+    botToken: 'MTUxNjA4NTM4MjI4MDkwODkyMQ.GBL72H.-ljAVIPzm4GK0VJDPAh-7kJgFdYwAjF8ZztvQk',
+    discordApplicationId: '1516085382280908921',
     port: 3000,
 };
